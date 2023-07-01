c8ae6eda467b63b5a7ee84b496f6ebca77df1b45
AoyonDev <aoyonislam010@gmail.com>
2023-07-01 19:27:00 +0600

    ENHANCED

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-
+// up button 
 $(function(){
   $('.top-scroll-btn').on("click",function(){
     $("html,body").animate({scrollTop:0}, 1000)
@@ -13,10 +13,6 @@ $(function(){
     }
   })
 })
-
-
-
-
 
 
   /* -- Glow effect -- */
@@ -34,62 +30,7 @@ window.onpointermove = event => {
 }
 })
 
-
-// const images = document.getElementsByClassName("image");
-
-// let globalIndex = 0,
-//     last = { x: 0, y: 0 };
-
-// const activate = (image, x, y) => {
-//   image.style.left = `${x}px`;
-//   image.style.top = `${y}px`;
-//   image.style.zIndex = globalIndex;
-
-//   image.dataset.status = "active";
-
-//   last = { x, y };
-// }
-
-// const distanceFromLast = (x, y) => {
-//   return Math.hypot(x - last.x, y - last.y);
-// }
-
-// const handleOnMove = e => {
-//   if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
-//     const lead = images[globalIndex % images.length],
-//           tail = images[(globalIndex - 5) % images.length];
-
-//     activate(lead, e.clientX, e.clientY);
-
-//     if(tail) tail.dataset.status = "inactive";
-    
-//     globalIndex++;
-//   }
-// }
-
-// window.onmousemove = e => handleOnMove(e);
-
-// window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-
-
-
-
-
-
-
-// common counter up
-
-// $(document).ready(function () {
-//     $(".main-counter").counterUp({
-//       delay: 10,
-//       time: 700,
-//     });
-//   });
-
-
-
-
+// counter up 
 $('.main-counter').counterUp({
   delay: 10,
   time: 1000
